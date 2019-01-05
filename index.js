eadf01e644adfc8242788054fff0a9f499565a21
robinsonk <43895274+robinsonk@users.noreply.github.com>
2019-01-04 23:24:11 -0700

    Added Google Books API functionality
    Added Google Books API functionality to populate general information for the user's search

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
 
-const apiKey= 'AIzaSyAuWUVv36LyE1npxUfX3OhWRc7n7ihYz4c';
-const searchURL = 'https://www.googleapis.com/youtube/v3/search'
+//Youtube API Key and endpoints
+//Youtube uses %20 to connect spaces 
+const youtubeApiKey= 'AIzaSyAuWUVv36LyE1npxUfX3OhWRc7n7ihYz4c';
+const youtubeSearchURL = 'https://www.googleapis.com/youtube/v3/search';
+
+//Google Books API Key and endpoint
+//Google Books uses %20 to connect spaces 
+const googleSearchURL = 'https://www.googleapis.com/books/v1/volumes?q=';
+const googleApiKey = 'CRt642z4e40YBentkuBMQ';
 
 function formListener() {
     console.log('The formListener function is running \nWaiting for user input...');
@@ -10,6 +17,7 @@ function formListener() {
         event.preventDefault();
         $('.youtube-results').empty();
         $('.results-header').empty();
+        $('.book-details').empty();
         console.log('The form has collected data');
 
         let titleField = $('#title').val()
@@ -22,15 +30,45 @@ function formListener() {
         <h2>Showing results for ${titleField} ${authorField}</h2>
         `);
 
-        apiCall(title, author);
+        youtubeApiCall(title, author);
+        googleBooksApiCall(title, author);
     })
 }
 
-function apiCall(title, author) {
-    console.log('The apiCall function is running...')
-    console.log(`${searchURL}?key=${apiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=3&type=video`);
+//Pulling basic book info from Google Books API for info section --------------------------------------------------------------------------
+
+function googleBooksApiCall(title, author) {
+    console.log('The googleBooksApiCall function is running...');
+    console.log(`${googleSearchURL}${title}%20${author}`);
+ //Pulls data from Google Books API
+    fetch(`${googleSearchURL}${title}%20${author}`)
+    .then(response => response.json())
+    .then(responseJson2 =>
+        displayBookData(responseJson2))
+        .catch(error => alert('something went wrong'));
+}
+
+function displayBookData(responseJson2) {
+    console.log('The displayBookData function is running...')
+    console.log(responseJson2);
+ //Display general book information for user's search
+    $('.book-details').append(`
+    <img src="${responseJson2.items[0].volumeInfo.imageLinks.thumbnail}" alt="${responseJson2.items[0].volumeInfo.title} cover photo">
+    <h2>Title: ${responseJson2.items[0].volumeInfo.title}</h2>
+    <h3>Author: ${responseJson2.items[0].volumeInfo.authors}</h3>
+    <h4>Genre: ${responseJson2.items[0].volumeInfo.categories}</h4>
+    <p>Blurb: ${responseJson2.items[0].searchInfo.textSnippet}</p><br><br><br>
+    `);    
+}
+
+
+//Gathering Review Results from YT and GB ---------------------------------------------------------------------------------------------------
+
+function youtubeApiCall(title, author) {
+    console.log('The youtubeApiCall function is running...')
+    console.log(`${youtubeSearchURL}?key=${youtubeApiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=3&type=video`);
  //Fetch info from the api, based on form data
-    fetch(`${searchURL}?key=${apiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=3&type=video`)
+    fetch(`${youtubeSearchURL}?key=${youtubeApiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=3&type=video`)
     .then(response => response.json())
     .then(responseJson =>
         displayResults(responseJson))
