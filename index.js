e5fd1cdcf5d2b327be91536a4f758e6ccd90a457
robinsonk <43895274+robinsonk@users.noreply.github.com>
2019-01-05 17:01:01 -0700

    Added initial css, info toggle functionality
    Added initial basic CSS styling as well as ability to toggle extended book details with JQuery

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// -----------TO DO---------------
+// -Plan for edge cases, errors, cases where things don't work as they should (404s, etc)
+
 //Youtube API Key and endpoints
 //Youtube uses %20 to connect spaces 
 const youtubeApiKey= 'AIzaSyAuWUVv36LyE1npxUfX3OhWRc7n7ihYz4c';
@@ -20,18 +23,26 @@ function formListener() {
         $('.book-details').empty();
         console.log('The form has collected data');
 
-        let titleField = $('#title').val()
-        let authorField = $('#author').val()
+        let titleField = $('#title').val();
+        let authorField = $('#author').val();
+        let resultLimit = $('#result-limit').val();
         let title = titleField.replace(/[, ]+/g, '%20');
         let author = authorField.replace(/[, ]+/g, '%20');
-        console.log(`The user wants to see reviews of ${titleField} by ${authorField}.`);
+        console.log(`The user wants to see ${resultLimit} reviews of ${titleField} by ${authorField}.`);
 
-        $('.results-header').append(`
-        <h2>Showing results for ${titleField} ${authorField}</h2>
-        `);
+        if (resultLimit < 1 || resultLimit > 10) {
+            alert('Please enter a number between 1 and 10');
+        }
 
-        youtubeApiCall(title, author);
-        googleBooksApiCall(title, author);
+        else {
+
+            $('.results-header').append(`
+            <h2>Showing ${resultLimit} results for ${titleField} ${authorField}</h2>
+            `);
+
+            youtubeApiCall(title, author, resultLimit);
+            googleBooksApiCall(title, author, resultLimit);
+        }
     })
 }
 
@@ -48,27 +59,47 @@ function googleBooksApiCall(title, author) {
         .catch(error => alert('something went wrong'));
 }
 
+
 function displayBookData(responseJson2) {
     console.log('The displayBookData function is running...')
     console.log(responseJson2);
  //Display general book information for user's search
     $('.book-details').append(`
     <img src="${responseJson2.items[0].volumeInfo.imageLinks.thumbnail}" alt="${responseJson2.items[0].volumeInfo.title} cover photo">
-    <h2>Title: ${responseJson2.items[0].volumeInfo.title}</h2>
+    <h2>${responseJson2.items[0].volumeInfo.title}</h2>
     <h3>Author: ${responseJson2.items[0].volumeInfo.authors}</h3>
-    <h4>Genre: ${responseJson2.items[0].volumeInfo.categories}</h4>
-    <p>Blurb: ${responseJson2.items[0].volumeInfo.description}</p><br><br><br>
-    `);    
+    <button class="expand" type="button">Show/hide book details</button>
+    <div class="info-dropdown">
+        <h4>Publisher: ${responseJson2.items[0].volumeInfo.publisher}</h4>
+        <h4>Publish Date: ${responseJson2.items[0].volumeInfo.publishedDate}</h4>
+        <h4>Genre: ${responseJson2.items[0].volumeInfo.categories}</h4>
+        <h4>Google Books Rating: ${responseJson2.items[0].volumeInfo.averageRating}/5</h4>
+        <h4>Page Count: ${responseJson2.items[0].volumeInfo.pageCount}</h4>
+        <h4><a target="_blank" href="${responseJson2.items[0].volumeInfo.previewLink}">Preview the book here on Google Books</a></h4>
+        <h4><a target="_blank" href="${responseJson2.items[0].saleInfo.buyLink}">Purchase on Google Books</a></h4>
+        <p>Blurb: ${responseJson2.items[0].volumeInfo.description}</p><br><br><br>
+    </div>
+    `); 
+    
+    showBookData();
+}
+
+function showBookData() {
+    console.log('Show book data has ran');
+    $('.info-dropdown').hide();
+    $('.expand').click(function() {
+        $('.info-dropdown').toggle();
+    })
 }
 
 
 //Gathering and displaying Review Results from YT and GB --------------------------------------------------------------------------------------
 
-function youtubeApiCall(title, author) {
+function youtubeApiCall(title, author, resultLimit) {
     console.log('The youtubeApiCall function is running...')
-    console.log(`${youtubeSearchURL}?key=${youtubeApiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=3&type=video`);
+    console.log(`${youtubeSearchURL}?key=${youtubeApiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=${resultLimit}&type=video`);
  //Fetch info from the api, based on form data
-    fetch(`${youtubeSearchURL}?key=${youtubeApiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=3&type=video`)
+    fetch(`${youtubeSearchURL}?key=${youtubeApiKey}&q=book%20review%20${title}%20${author}&part=snippet&maxResults=${resultLimit}&type=video`)
     .then(response => response.json())
     .then(responseJson =>
         displayResults(responseJson))
@@ -78,24 +109,22 @@ function youtubeApiCall(title, author) {
 function displayResults(responseJson) {
     console.log('The displayResults function is running...')
     console.log(responseJson);
- //Display the results from the apiCall function and display them in the DOM
+ //Display the results from the youtubeapiCall function and display them in the DOM
     for (let i = 0; i < responseJson.items.length; i++)
     $('.youtube-results').append(`
-    <iframe id="ytplayer" type="text/html" width="640" height="360"
-        src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}?autoplay=1"
+    <iframe id="ytplayer" type="text/html" 
+        src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}?autoplay=0"
         frameborder="0">
     </iframe>
     <h3>${responseJson.items[i].snippet.title}</h3>
     <p>${responseJson.items[i].snippet.description}</p><br><br><br>
     `);
-    //Display embedded YT video
 }
 
 function runApp() {
     console.log('The Book Review App is running');
  //Run the app on page load
     formListener();
-
 }
 
 $(runApp);
